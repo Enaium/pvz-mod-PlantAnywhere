cc947c7ca44330c571dcb07148faa03437ab8fb8
Enaium <wyy-666@hotmail.com>
2025-11-23 12:22:08 +0800

    Update

--- PlantAnywhere/Properties/AssemblyInfo.cs
+++ PlantAnywhere/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using PlantAnywhere;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(ModEntry), "PlantAnywhere", "1.0.0", "Enaium")]
+[assembly: MelonInfo(typeof(ModEntry), "PlantAnywhere", "1.0.1", "Enaium")]
